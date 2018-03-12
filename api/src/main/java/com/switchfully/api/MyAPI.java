e2804d29a750a347994249a4c81cb005bd3208e4
Simon Bouvy <simonbouvy@outlook.com>
2018-03-12 14:15:50 +0100

    dependencies should use the plugins as well

--- api/src/main/java/com/switchfully/api/MyAPI.java
+++ api/src/main/java/com/switchfully/api/MyAPI.java
@@ -1,6 +1,9 @@
 package com.switchfully.api;
 
+import com.google.common.collect.Lists;
 import com.switchfully.domain.person.Person;
+
+import java.util.ArrayList;
 
 public class MyAPI {
 
@@ -9,6 +12,9 @@ public class MyAPI {
         Person personOne = new Person("Marc", 36);
         Person personTwo = new Person("Timmy", 8);
 
-
+        ArrayList<Person> personList = Lists.newArrayList(personOne,personTwo);
+        for (Person person: personList) {
+            System.out.println(person);
+        }
     }
 }
