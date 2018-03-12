05c31fe46a9f0c58f380bd1d91babfd2e624ec3b
Simon Bouvy <simonbouvy@outlook.com>
2018-03-12 09:29:37 +0100

    second commit

--- api/src/main/java/com/switchfully/api/MyAPI.java
+++ api/src/main/java/com/switchfully/api/MyAPI.java
@@ -1,4 +1,14 @@
 package com.switchfully.api;
 
+import com.switchfully.domain.person.Person;
+
 public class MyAPI {
+
+    public static void main(String[] args) {
+
+        Person personOne = new Person("Marc", 36);
+        Person personTwo = new Person("Timmy", 8);
+
+
+    }
 }
--- domain/src/main/java/com/switchfully/domain/person/Person.java
+++ domain/src/main/java/com/switchfully/domain/person/Person.java
@@ -1,4 +1,17 @@
 package com.switchfully.domain.person;
 
 public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "My name is " + name + " I'm " + age + "years old.";
+    }
 }
